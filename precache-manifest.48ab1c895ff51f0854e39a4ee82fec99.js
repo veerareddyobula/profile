7ca7896e32804aa724affb93a4fb5a256ec570d0
veera <v-vobulareddy@expedia.com>
2020-03-04 20:30:32 +0530

    Updates

---
+++ precache-manifest.48ab1c895ff51f0854e39a4ee82fec99.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e5e2873f18a388799bbc",
+    "url": "/profile/static/css/main.7d9fad79.chunk.css"
+  },
+  {
+    "revision": "e5e2873f18a388799bbc",
+    "url": "/profile/static/js/main.c7f63a5f.chunk.js"
+  },
+  {
+    "revision": "189fd871db148202566e",
+    "url": "/profile/static/js/runtime~main.cc8686f1.js"
+  },
+  {
+    "revision": "162f251c2e1b4d27641c",
+    "url": "/profile/static/css/2.f0f81c3d.chunk.css"
+  },
+  {
+    "revision": "162f251c2e1b4d27641c",
+    "url": "/profile/static/js/2.44044d53.chunk.js"
+  },
+  {
+    "revision": "a8c21a912268e7145303fcbc0a9b40c6",
+    "url": "/profile/static/media/Veera_Resume.a8c21a91.pdf"
+  },
+  {
+    "revision": "825f15aa90263c46d76578ba81826926",
+    "url": "/profile/static/media/Veera_Resume.825f15aa.doc"
+  },
+  {
+    "revision": "5b6d1d402eabf65bc61d757b652ac7a3",
+    "url": "/profile/static/media/thimble-pattern@2x.5b6d1d40.png"
+  },
+  {
+    "revision": "d2c37cd53c1b507b42079d7cb54ce9db",
+    "url": "/profile/static/media/interview_preparation.d2c37cd5.png"
+  },
+  {
+    "revision": "6f980832dc1bd75cb3cf65b3b2f58af0",
+    "url": "/profile/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.4c27084d66adeb399d0d2a2414ea9d6c.js"
+  "/profile/precache-manifest.48ab1c895ff51f0854e39a4ee82fec99.js"
 );
 
 workbox.clientsClaim();
