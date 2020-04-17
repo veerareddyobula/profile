950120c7ab2ca305afa30aa45490b99745ad9565
veera <v-vobulareddy@expedia.com>
2020-04-17 14:56:40 +0530

    Updates

--- precache-manifest.677e52c54424cd4ae610fbec51666877.js
+++ precache-manifest.677e52c54424cd4ae610fbec51666877.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "66ba46dbe733bc537dc4",
+    "revision": "cbc6930a44138a4cc8ac",
     "url": "/profile/static/css/main.4a009afc.chunk.css"
   },
   {
-    "revision": "66ba46dbe733bc537dc4",
-    "url": "/profile/static/js/main.c1bd0e68.chunk.js"
+    "revision": "cbc6930a44138a4cc8ac",
+    "url": "/profile/static/js/main.caa01fd3.chunk.js"
   },
   {
     "revision": "189fd871db148202566e",
     "url": "/profile/static/js/runtime~main.cc8686f1.js"
   },
   {
-    "revision": "747d40c5be4efdedd07a",
+    "revision": "1916e4c665a75968b1c3",
     "url": "/profile/static/css/2.dcfab514.chunk.css"
   },
   {
-    "revision": "747d40c5be4efdedd07a",
-    "url": "/profile/static/js/2.155fdb19.chunk.js"
+    "revision": "1916e4c665a75968b1c3",
+    "url": "/profile/static/js/2.9d3190c5.chunk.js"
   },
   {
     "revision": "1b3ef0388cb392445094d06516d6c48f",
@@ -28,15 +28,15 @@ self.__precacheManifest = [
     "url": "/profile/static/media/Veera_Resume.2fc769d8.doc"
   },
   {
-    "revision": "d2c37cd53c1b507b42079d7cb54ce9db",
-    "url": "/profile/static/media/interview_preparation.d2c37cd5.png"
-  },
-  {
     "revision": "5b6d1d402eabf65bc61d757b652ac7a3",
     "url": "/profile/static/media/thimble-pattern@2x.5b6d1d40.png"
   },
   {
-    "revision": "79d61dc576433df33f45d8fbc0741655",
+    "revision": "d2c37cd53c1b507b42079d7cb54ce9db",
+    "url": "/profile/static/media/interview_preparation.d2c37cd5.png"
+  },
+  {
+    "revision": "13d12a59dd2719bf742512b194933e47",
     "url": "/profile/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/profile/precache-manifest.125a5f00fe9b6f6c9308ff3329560664.js"
+  "/profile/precache-manifest.677e52c54424cd4ae610fbec51666877.js"
 );
 
 workbox.clientsClaim();
